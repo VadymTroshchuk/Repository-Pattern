54d2bceb3c7f4473a29fdb7e3e0304e07ddbecb1
Vadym Troshchuk <vadimtrosuk01@gmail.com>
2023-11-18 23:37:18 +0200

    add a repository

--- app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -2,7 +2,17 @@
 
 package com.example.android.devbyteviewer.repository
 
-/**
- * Repository for fetching devbyte videos from the network and storing them on disk
- */
-// TODO: Implement the VideosRepository class
+import com.example.android.devbyteviewer.database.VideosDatabase
+import com.example.android.devbyteviewer.network.DevByteNetwork
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class VideosRepository(private val database: VideosDatabase) {
+
+    suspend fun refreshVideos() {
+        withContext(Dispatchers.IO) {
+            val playlist = DevByteNetwork.devbytes.getPlaylist()
+            database.videoDao.insertAll(playlist.asDatabaseModel())
+        }
+    }
+}
